92c8050c8b5770c33e8fce67779ec806a5e3419d
Ben Obame <obameben@yahoo.fr>
2024-11-28 08:24:51 +0200

    Update 4-payment.test.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,20 +1,19 @@
 const sinon = require('sinon');
+const expect = require('chai').expect;
 const Utils = require('./utils');
-const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
-    const bigBrother = sinon.spy(console);
-    const dummy = sinon.stub(Utils, 'calculateNumber');
+  it('should call calculateNumber with the correct arguments', () => {
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10); // Stub always returns 10
+    const spy = sinon.spy(console, 'log'); // Spy on console.log
 
-    dummy.returns(10);
     sendPaymentRequestToApi(100, 20);
-    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
-    expect(dummy.callCount).to.be.equal(1);
-    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
-    expect(bigBrother.log.callCount).to.be.equal(1);
-    dummy.restore();
-    bigBrother.log.restore();
+
+    expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spy.calledWith('The total is: 10')).to.be.true;
+
+    stub.restore(); // Restore original function
+    spy.restore();
   });
 });
