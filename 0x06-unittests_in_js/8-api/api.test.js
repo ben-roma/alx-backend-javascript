977c782830ab3653f32b88b8689cf9e643bd885a
Ben Obame <obameben@yahoo.fr>
2024-11-28 08:41:45 +0200

    Update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,17 @@
 const request = require('request');
-const { expect } = require('chai');
+const expect = require('chai').expect;
 
-describe('API integration test', () => {
-  const API_URL = 'http://localhost:7865';
+describe('Index page', () => {
+  it('should return status code 200', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
 
-  it('GET / returns correct response', (done) => {
-    request.get(`${API_URL}/`, (_err, res, body) => {
-      expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Welcome to the payment system');
+  it('should return "Welcome to the payment system"', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
