ad52a296c6ceeb61069d0548aece3dc2a7fd01db
Ben Obame <obameben@yahoo.fr>
2024-11-28 08:24:46 +0200

    Update 4-payment.js

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,8 +1,8 @@
 const Utils = require('./utils');
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${totalCost}`);
+  const calculatedAmount = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${calculatedAmount}`);
 };
 
 module.exports = sendPaymentRequestToApi;
